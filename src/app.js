f020acf77d788fe696347fe77781f1268ce954e4
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-17 14:41:44 +0700

    feat(routes): add the main router structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import { authRouter, commentRouter, postRouter, userRouter } from './routes';
 
 const app = express();
+
+app.use('/auth', authRouter);
+app.use('/posts', postRouter);
+app.use('/users', userRouter);
+app.use('/comments', commentRouter);
 
 export default app;
