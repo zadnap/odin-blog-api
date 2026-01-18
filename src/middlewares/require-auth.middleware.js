ddc64f5b33e1cf99d758c77c35166706023aadab
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-18 15:42:33 +0700

    feat(auth): add login route with jwt strategy

---
+++ src/middlewares/require-auth.middleware.js
@@ -0,0 +1,20 @@
+import passport from '../lib/passport.js';
+
+const requireAuth = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        message: info?.message || 'Unauthorized',
+      });
+    }
+
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
+export default requireAuth;
