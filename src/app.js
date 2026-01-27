575ea95334e2bc989254f8717f99d0263a2df65e
Zadnap <88957683+zadnap@users.noreply.github.com>
2026-01-27 15:11:33 +0700

    chore(config): add cors configuration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import authRouter from './routes/auth.routes.js';
 import postRouter from './routes/post.routes.js';
 import { errorHandler } from './middlewares/index.js';
+import { allowedOrigins } from './config/cors.config.js';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS blocked'));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
